a40e8f4a39e61fa5590c905cc553cb7f4f46eb6d
Blake Gotting and chris rudnicky <chris@>
2018-10-24 10:46:43 -0700

    Initial commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+//Business Logic
+var vowels = ["a","e","i","o","u"];
+var consonants = ["q","w","r","t","y","p","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+
+
+
+
+
+
+//User Interface
 $(document).ready(function() {
 
-});
+});atom .
